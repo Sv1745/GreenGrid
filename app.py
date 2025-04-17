bec304e722b3a3f4623b80925d47dbdcd92f3b1d
svsm <srivathsamurthys.1745@gmail.com>
2025-04-17 13:56:53 +0530

    500 fux

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Flask app config
-app = Flask(__name__, template_folder="../templates", static_folder="../static")
+app = Flask(__name__, template_folder="templates", static_folder="static")
 
 # Supabase config
 supabase_url = "https://yhqdszazpxmcetpxnyjx.supabase.co"
